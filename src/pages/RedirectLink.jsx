5df90b9333caabd906eb31f2b3540b5dac587f87
Somenath Choudhury <81456073+somenath203@users.noreply.github.com>
2024-08-28 01:33:50 +0530

    Update RedirectLink.jsx

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,22 +1,18 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import BarLoader from 'react-spinners/BarLoader';
 
 import useFetch from "@/hooks_for_api_calls/use-fetch";
 import { findTargetUrlForRedirection, storeClicksDetailsOfUrlsOnClick } from "@/supabase_apis/apiUrlClicks";
 
-
 const RedirectLink = () => {
-
   const { id } = useParams();
-
 
   const {
     data: targetUrlData,
     loading: targetUrlLoading,
     executeCallbackFunction: executeFindTargetUrlFunction
   } = useFetch(findTargetUrlForRedirection, id);
-
 
   const {
     loading: storeClicksLoading,
@@ -26,44 +22,30 @@ const RedirectLink = () => {
     original_url: targetUrlData?.original_url
   });
 
+  const hasStoredClicks = useRef(false); // A flag to ensure the clicks are stored only once
 
   useEffect(() => {
-
     executeFindTargetUrlFunction();
-
   }, []);
 
-
   useEffect(() => {
-
-    if (!targetUrlLoading && targetUrlData) {
-
+    if (!targetUrlLoading && targetUrlData && !hasStoredClicks.current) {
       executeStoreClicksFunction();
-      
+      hasStoredClicks.current = true; 
     }
-
-  }, [targetUrlLoading]);
-
+  }, [targetUrlLoading, targetUrlData]); 
 
   if (storeClicksLoading || targetUrlLoading) {
-
     return (
       <>
-
         <BarLoader width={'100%'} color="#36d7b7" />
-
         <br />
-
-        Redirecting... 
-        
+        Redirecting...
       </>
     )
-
-  } 
-
+  }
 
   return null;
-
 }
 
 export default RedirectLink;

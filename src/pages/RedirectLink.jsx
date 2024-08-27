9f41b66395a58a8cb74379841558d1507625f544
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-08-28 01:51:08 +0530

    commited

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -5,18 +5,14 @@ import BarLoader from 'react-spinners/BarLoader';
 import useFetch from "@/hooks_for_api_calls/use-fetch";
 import { findTargetUrlForRedirection, storeClicksDetailsOfUrlsOnClick } from "@/supabase_apis/apiUrlClicks";
 
-
 const RedirectLink = () => {
-
   const { id } = useParams();
-
 
   const {
     data: targetUrlData,
     loading: targetUrlLoading,
     executeCallbackFunction: executeFindTargetUrlFunction
   } = useFetch(findTargetUrlForRedirection, id);
-
 
   const {
     loading: storeClicksLoading,
@@ -26,44 +22,27 @@ const RedirectLink = () => {
     original_url: targetUrlData?.original_url
   });
 
-
   useEffect(() => {
-
     executeFindTargetUrlFunction();
-
   }, []);
 
-
   useEffect(() => {
-
     if (!targetUrlLoading && targetUrlData) {
-
       executeStoreClicksFunction();
-      
     }
-
-  }, [targetUrlLoading]);
-
+  }, [targetUrlLoading, targetUrlData]);
 
   if (storeClicksLoading || targetUrlLoading) {
-
     return (
       <>
-
         <BarLoader width={'100%'} color="#36d7b7" />
-
         <br />
-
-        Redirecting... 
-        
+        Redirecting...
       </>
-    )
-
-  } 
-
+    );
+  }
 
   return null;
-
-}
+};
 
 export default RedirectLink;

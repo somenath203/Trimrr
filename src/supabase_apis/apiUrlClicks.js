a7d05640c0dd6e69238ba9f32b11d6fc5d4c5c72
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-08-28 04:27:27 +0530

    commited

--- src/supabase_apis/apiUrlClicks.js
+++ src/supabase_apis/apiUrlClicks.js
@@ -1,79 +1,61 @@
-import { UAParser } from "ua-parser-js";
+import { UAParser } from 'ua-parser-js';
 
-import supabase from "@/db/supabase";
-
+import supabase from '@/db/supabase';
 
 const parser = new UAParser();
 
-
 export const findTargetUrlForRedirection = async (short_url_or_custom_url) => {
+  try {
+    if (short_url_or_custom_url) {
+      const { data, error } = await supabase
+        .from('urls')
+        .select('id, original_url')
+        .or(
+          `short_url.eq.${short_url_or_custom_url}, custom_url.eq.${short_url_or_custom_url}`
+        )
+        .single();
 
-    try {
-
-        const { data, error } = await supabase
-                                       .from('urls')
-                                       .select('id, original_url')
-                                       .or(`short_url.eq.${short_url_or_custom_url}, custom_url.eq.${short_url_or_custom_url}`)
-                                       .single();
-        
-        if (error) {
-
-            console.log(error);
-
-            throw new Error(error?.message);
-
-        }
-
-
-        return data;
-
-
-    } catch (error) {
-        
+      if (error) {
         console.log(error);
 
-        return error;
+        throw new Error(error?.message);
+      }
 
+      return data;
     }
+  } catch (error) {
+    console.log(error);
 
-}
+    return error;
+  }
+};
 
+export const storeClicksDetailsOfUrlsOnClick = async ({
+  url_id,
+  original_url,
+}) => {
+  try {
+    if (url_id && original_url) {
+      const res_device = parser.getResult();
 
-export const storeClicksDetailsOfUrlsOnClick = async ({ url_id, original_url }) => {
+      const device_name = res_device.type || 'Desktop/Laptop';
 
-    try {
-        
-        const res_device = parser.getResult();        
+      const res_city_country = await fetch(`https://ipapi.co/json`);
 
-        const device_name = res_device.type || 'Desktop/Laptop';
+      const { city, country_name: country } = await res_city_country.json();
 
+      await supabase.from('clicks').insert({
+        url_id: url_id,
+        city_in_which_url_was_clicked: city,
+        country_in_which_url_was_clicked: country,
+        device_in_which_url_was_clicked: device_name,
+      });
 
-        const res_city_country = await fetch(`https://ipapi.co/json`);
-
-        const { city, country_name: country } = await res_city_country.json();
-
-
-        await supabase
-              .from('clicks')
-              .insert({
-                url_id: url_id,
-                city_in_which_url_was_clicked: city,
-                country_in_which_url_was_clicked: country,
-                device_in_which_url_was_clicked: device_name
-              });
-
-        
-        window.location.href = original_url;
-
-
-    } catch (error) {
-        
-        console.log(error);
-
-        return error;
-
+      window.location.href = original_url;
     }
+  } catch (error) {
+    console.log(error);
 
-}
-
-
+    return error;
+  }
+};
